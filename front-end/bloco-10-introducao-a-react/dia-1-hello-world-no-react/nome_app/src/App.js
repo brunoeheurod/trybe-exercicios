03cebeb6a6213e01553afda584f5a837fef53b9d
Bruno Rodrigues <bruno.eheurod@gmail.com>
2022-08-01 15:39:25 -0300

    exercicios 10.1

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome_app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/nome_app/src/App.js
@@ -1,16 +1,32 @@
+import React from 'react';
+import catImage from './catImage.jpg'
 import './App.css';
+// const array = [ 'caminhar', 'correr', 'nadar', 'viajar', 'estudar']
 
-const Task = (value) => {
-  return (
-    <li>{value}</li>
-  );
+// const Task = (value) => {
+//   return (
+//     <li>{value}</li>
+//   );
+// }
+
+// const List = (array) => {
+//   return (
+//     <ol id='list'>{array.map((toDo) => Task(toDo))}</ol>
+//   );
+// }
+
+
+
+class Image extends React.Component {
+  render() {
+    return <img src={this.props.source} alt={this.props.alternativeText} />;
+  }
 }
-const array = [ 'caminhar', 'correr', 'nadar', 'viajar', 'estudar']
-const olElement = <ol>{array.map((toDo) => Task(toDo))}</ol>
 
 const App = () => {
   return (
-    olElement
+    // List(array)
+    <Image source={catImage} alternativeText='isso é uma batata'/>
   );
 }
 
